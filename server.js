5d8e7d6f5fa072c3fdac0178b4ec5154f055601f
Arpita2265 <arpita.t144@gmail.com>
2025-12-02 20:43:23 +0530

    update

--- server.js
+++ server.js
@@ -23,19 +23,19 @@ const server = http.createServer((req, res) => {
     }
 
 
-    if (req.url.toLocaleLowerCase() === '/' || req.url.toLocaleLowerCase() === '/home' ) {
+    if (req.url.toLocaleLowerCase() === '/Assignment6-node' || req.url.toLocaleLowerCase() === '/home' ) {
         filePath = path.join(__dirname, 'Home.html');
 
 
-    }else if(req.url.toLocaleLowerCase() === '/services'){
+    }else if(req.url.toLocaleLowerCase() === '/Assignment6-node/services'){
 
- filePath = path.join(__dirname, 'services.html');
+ filePath = path.join(__dirname, '/Assignment6-node/services.html');
 
-    }else if(req.url.toLocaleLowerCase() === '/about'){
+    }else if(req.url.toLocaleLowerCase() === '/Assignment6-node/about'){
 
 filePath = path.join(__dirname, 'about.html');
 
-    }else if (req.url.toLocaleLowerCase() === '/contact'){
+    }else if (req.url.toLocaleLowerCase() === '/Assignment6-node/contact'){
 
          filePath = path.join(__dirname, 'contact.html');
 
